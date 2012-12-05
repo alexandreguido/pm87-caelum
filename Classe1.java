d9163b8ea9d9137d9f1b62e9ddf90a3d44a47b96
alexandreguido <agil2910@caelum031.(none)>
2012-12-05 17:30:31 -0200

    commit 3

--- Classe1.java
+++ Classe1.java
@@ -1,5 +1,5 @@
 public class Classe1{
 	public static void main(String[] args){
-		System.out.println("Exemplo de teste de uso do git2");
+		System.out.println("Exemplo de teste de uso do git3");
 	}
 }
